8cf9af6fab1d15f27966076611d3f1d3a3398fb0
moneill-mw <moneill@merchantwarehouse.com>
2014-01-10 12:46:37 +0000

    Enabled NuGet Package Restore for the solution Updated .gitignore to ignore NuGet packages (but not the Nuget repositories.config) Installed the SpecFlow.NUnit NuGet package and dependent packages Added Domain.Specs project to try out SpecFlow/Gherkin BDD tests

---
+++ Domain.Specs/User.feature
@@ -0,0 +1,10 @@
+﻿Feature: User
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Basic Creation
+	Given a username of "marko"
+	When I create a User
+	Then the username should be "marko"
---
+++ Domain.Specs/UserSteps.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Domain.Specs
+{
+    [Binding]
+    public class UserSteps
+    {
+        private string _username;
+        private User _user;
+        [Given(@"a username of ""(.*)""")]
+        public void GivenAUsernameOf(string p0)
+        {
+            _username = p0;
+        }
+        
+        [When(@"I create a User")]
+        public void WhenICreateAUser()
+        {
+            _user = new User(_username);
+        }
+        
+        [Then(@"the username should be ""(.*)""")]
+        public void ThenTheUsernameShouldBe(string p0)
+        {
+            Assert.That(_user.Name, Is.EqualTo(_username));
+        }
+    }
+}
